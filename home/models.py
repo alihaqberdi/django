de4620dd30e647757cbe82b71c8113b94cb6840a
Alijon Xakberdiyev <107967097+AlisheyxPy@users.noreply.github.com>
2023-01-19 23:43:32 +0500

    Update models.py

--- home/models.py
+++ home/models.py
@@ -4,17 +4,6 @@ from django.db import models
 
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        return self.name
-
-
-class odam(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    savat = models.ManyToManyField(Product,)
 
 
 
