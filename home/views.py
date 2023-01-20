74fbf5c0bc2d6f9ff0fc41ccd866c0a09d54fba2
Alijon Xakberdiyev <107967097+AlisheyxPy@users.noreply.github.com>
2023-01-20 22:36:05 +0500

    Update views.py

--- home/views.py
+++ home/views.py
@@ -5,8 +5,23 @@ from .serializers import WomenSerilizers
 # Create your views here.
 
 
-class WomenApiView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerilizers
+class WomenApiView(APIView):
+    def get(self, request):
+        lst = Women.objects.all().values()
+        return Response({"get" : list(lst)})
+
+    def post(self, request):
+        new_user = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id'],
+        )
+        return Response({"post" :model_to_dict(new_user)})
+
+
+
+# class WomenApiView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerilizers
 
 
