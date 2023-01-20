26cd4241dabeb6c4b9cab54f4aab11fbd2c89313
Alijon Xakberdiyev <107967097+AlisheyxPy@users.noreply.github.com>
2023-01-20 21:50:00 +0500

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Product, odam
+from  .models import Women, Categories
 # Register your models here.
 
-admin.site.register([Product, odam])
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = 'title', 'content', 'time_create', 'time_update', 'is_published', 'cat'
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = 'name',
